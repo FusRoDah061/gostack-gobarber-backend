b13e0d02a334c0be9d0d4298cbd5e99e9f8003e4
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-10-21 18:41:43 -0300

    Ajustando projeto para deploy no heroku

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,37 @@
 import { createConnections } from 'typeorm';
 
-// Busca pelo ormconfig.json
-createConnections();
+const rootDir = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
+const fileExtension = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
+
+// Caso não seja informado parêmtro, irá busca pelo ormconfig.json
+createConnections([
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DEFAULT_DATABASE_HOST,
+    port: Number(process.env.DEFAULT_DATABASE_PORT || 5432),
+    username: process.env.DEFAULT_DATABASE_USER,
+    password: process.env.DEFAULT_DATABASE_PASSWORD,
+    database: process.env.DEFAULT_DATABASE_NAME,
+    entities: [
+      `./${rootDir}/modules/**/infra/typeorm/entities/*.${fileExtension}`,
+    ],
+    migrations: [
+      `./${rootDir}/shared/infra/typeorm/migrations/*.${fileExtension}`,
+    ],
+    cli: {
+      migrationsDir: `./${rootDir}/shared/infra/typeorm/migrations`,
+    },
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.MONGO_DATABASE_HOST,
+    port: Number(process.env.MONGO_DATABASE_PORT || 27017),
+    database: process.env.MONGO_DATABASE_NAME,
+    useUnifiedTopology: true,
+    entities: [
+      `./${rootDir}/modules/**/infra/typeorm/schemas/*.${fileExtension}`,
+    ],
+  },
+]);
